66b41b402bc5768469c51d1acd2382c60b28f8ef
asifislam2 <111923685+asifislam2@users.noreply.github.com>
2022-09-15 14:44:10 +0600

    petroll project added

---
+++ js/tailwind.config.js
@@ -0,0 +1,37 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          red: 'red',
+          hover: '#FAFF14',
+          hover: '#6C6C6C',
+          lerncolor: '#F0F0F0',
+          furered: '#F40404',
+          gray: '#9E6969',
+          nevy: '#2707EF',
+          bordercolor: '#FFB800',
+          grayblack: '#282828',
+        },
+
+        maxWidth: {
+            contanier: '1000px',
+        },
+        fontFamily:{
+
+            popins:'Poppins',
+
+        },
+
+        backgroundImage:{
+
+            banner: "url('images/banner.png')",
+            image5: "url('images/img5.png')",
+            image6: "url('images/img6.png')",
+            moreimage: "url('images/moreone.png')",
+            moreimage: "url('images/moreone.png')",
+        }
+        
+      }
+    }
+  }
